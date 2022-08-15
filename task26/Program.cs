26d1e315a01563ac261cb106991eb88ab873773d
Alexjur5676 <alex9992142@mail.ru>
2022-08-16 02:13:35 +0700

    Add task_24_26_28_30

---
+++ task26/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (count = 0; number > 0; count++)
+{
+    number = number / 10;
+    //number /+ 10;
+}
+// while (number > 0)
+// {
+//     number = number / 10;
+//     count ++;
+// }
+Console.WriteLine(count);
