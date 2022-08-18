63372fba540b7239ec4f09854d84ea315c3b699e
Alexjur5676 <alex9992142@mail.ru>
2022-08-19 01:30:28 +0700

    Add task33

--- task33/Program.cs
+++ task33/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задайте массив. Напишите программу, которая определяет,
+//   присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// Массивы к задачам 32 и 33 можно сделать рандомными.
+//  В задаче 33 на экран выводится ТОЛЬКО одно сообщение: 
+//  либо "да", либо "нет".
+
+Console.WriteLine("Введите колличество элементов массива:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число для поиска его в массиве:");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[A];
+Random rand = new Random();
+int i;
+bool res = false;
+
+for (i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-9, 10);
+    if (array[i] == number)
+    {
+        res = true;
+        // Console.WriteLine($"Да - {number}");
+    }
+}
+
+Console.WriteLine(string.Join(", ", array));
+
+if (res == true)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
