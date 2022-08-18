1b7b07ff7dfd11989069f7cef568241588f2ae12
Alexjur5676 <alex9992142@mail.ru>
2022-08-19 02:51:02 +0700

    Add task37

--- task37/Program.cs
+++ task37/Program.cs
@@ -5,27 +5,28 @@
 // [6 7 3 6] -> 36 21
 
 
-// Console.WriteLine("Введите колличество элементов массива:");
-// int N = Convert.ToInt32(Console.ReadLine());
-int[] array = { 1, 2, 3, 4, 5 };
+Console.WriteLine("Введите колличество элементов массива:");
 
-// int[] array = new int[N];
-int[] arrayResult = new int[array.Length / 2 + array.Length % 2];
-// Random rand = new Random();
-// int res;
-int number = array.Length;
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+int[] resArray = new int[(array.Length + 1) / 2 ];
+Random rand = new Random();
 
-// for (int i = 0; i < array.Length; i++)
-// {
-//     array[i] = rand.Next(-9, 10);
-// }
-
-for (int i = 0; i < arrayResult.Length; i++)
+for (int i = 0; i < array.Length; i++)
 {
-    arrayResult[i] = array[i] * array[array.Length - 1 - i];
+    array[i] = rand.Next(-9, 10);
 }
-if (array.Length %2 != 0)
+
+for (int i = 0; i < resArray.Length; i++)
 {
-    arrayResult[arrayResult.Length - 1] = array[array.Length/2];
+    resArray[i] = array[i] * array[array.Length - 1 - i];
+
 }
-    Console.WriteLine(string.Join(", ", arrayResult));
+
+Console.WriteLine(string.Join(", ", array));
+
+if (array.Length %2 == 1)
+{
+    resArray[resArray.Length - 1] = array[array.Length/2];
+}
+Console.WriteLine(string.Join(", ", resArray));
