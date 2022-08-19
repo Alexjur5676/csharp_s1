103338b19d1bcb49e9aad52f026897567c37d488
Alexjur5676 <alex9992142@mail.ru>
2022-08-19 10:42:01 +0700

    Add task37

--- task37/Program.cs
+++ task37/Program.cs
@@ -6,27 +6,27 @@
 
 
 Console.WriteLine("Введите колличество элементов массива:");
-
 int N = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[N];
-int[] resArray = new int[(array.Length + 1) / 2 ];
-Random rand = new Random();
 
+Random rand = new Random();
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = rand.Next(-9, 10);
 }
 
+// int[] array = { 1, 2, 3, 4, 5 };
+
+int[] resArray = new int[(array.Length + 1) / 2];
+
 for (int i = 0; i < resArray.Length; i++)
 {
     resArray[i] = array[i] * array[array.Length - 1 - i];
-
 }
-
 Console.WriteLine(string.Join(", ", array));
 
-if (array.Length %2 == 1)
+if (array.Length % 2 == 1)
 {
-    resArray[resArray.Length - 1] = array[array.Length/2];
+    resArray[resArray.Length - 1] = array[array.Length / 2];
 }
 Console.WriteLine(string.Join(", ", resArray));
