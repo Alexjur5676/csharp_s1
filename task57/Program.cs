1d5f2c99449c396ef8b50a97ae1ac1a0cbfad78c
Alexjur5676 <alex9992142@mail.ru>
2022-08-30 03:46:06 +0700

    Add task_57_59_61

--- task57/Program.cs
+++ task57/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Составить частотный словарь элементов двумерного массива.
+//  Частотный словарь содержит информацию о том, 
+//  сколько раз встречается элемент входных данных.
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+// В нашей исходной матрице встречаются элементы от 0 до 9
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 9)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void PrintRepeatsOfElements(int[,] matrix)
+{
+    int[] repeats = new int[10];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            repeats[matrix[i, j]]++;
+        }
+    }
+
+    for (int i = 0; i < repeats.Length; i++)
+    {
+        if (repeats[i] > 0)
+        {
+            Console.WriteLine($"Количество повторений {i} = {repeats[i]}");
+        }
+    }
+}
+
+ 
+Console.Write("Введите m и n через Enter "); 
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+Console.WriteLine();
+PrintRepeatsOfElements(matrix);
